d7ccb5132bc23078e450386df4e88d53914d6253
sowsouleymane <souleymane.sow@eurostar.com>
2020-11-23 11:56:13 +0000

    fiexes alt errors for Song Component

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Song = ({ currentSong: { artist, song, cover } }) => {
+const Song = ({ currentSong: { artist, song, cover, name } }) => {
   return (
     <div className="song-container">
-      <img src={cover}></img>
+      <img alt={name} src={cover}></img>
       <h2>{artist}</h2>
       <h3>{song}</h3>
     </div>
